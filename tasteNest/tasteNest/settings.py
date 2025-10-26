dbbc355654bcdccb286495f5d6b34f3cffd490ed
Tejaswa Dayal <tejaswadayalsri@gmail.com>
2025-10-26 19:03:28 +0530

    Update staticfiles storage backend to use CompressedStaticFilesStorage for improved performance

--- tasteNest/tasteNest/settings.py
+++ tasteNest/tasteNest/settings.py
@@ -21,8 +21,8 @@ STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+     "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage", 
     },
 }
 
