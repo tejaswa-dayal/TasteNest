58c288e4d6cac27da6331b0f0e136de065d61671
Tejaswa Dayal <tejaswadayalsri@gmail.com>
2025-10-26 18:21:42 +0530

    Revert "Change static files storage backend to Whitenoise"
    This reverts commit 4c5944c0d34fdb907d3aa15d4f46d7ce0e0c472f.

--- tasteNest/tasteNest/settings.py
+++ tasteNest/tasteNest/settings.py
@@ -20,7 +20,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
